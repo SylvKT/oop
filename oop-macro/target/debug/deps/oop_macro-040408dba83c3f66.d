4aac90a700edfc0b6907636c12a0691eb10b29d3
SylvKT <contact@sylv.gay>
2023-12-28 11:51:31 -0500

    macros

---
+++ oop-macro/target/debug/deps/oop_macro-040408dba83c3f66.d
@@ -0,0 +1,5 @@
+/home/sylv/Projects/oop/oop-macro/target/debug/deps/liboop_macro-040408dba83c3f66.rmeta: src/lib.rs
+
+/home/sylv/Projects/oop/oop-macro/target/debug/deps/oop_macro-040408dba83c3f66.d: src/lib.rs
+
+src/lib.rs:
---
+++ oop-macro/target/debug/deps/oop_macro-5d1db199ff349968.d
@@ -0,0 +1,5 @@
+/home/sylv/Projects/oop/oop-macro/target/debug/deps/liboop_macro-5d1db199ff349968.rmeta: src/lib.rs
+
+/home/sylv/Projects/oop/oop-macro/target/debug/deps/oop_macro-5d1db199ff349968.d: src/lib.rs
+
+src/lib.rs:
